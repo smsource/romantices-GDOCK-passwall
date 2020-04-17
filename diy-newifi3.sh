fbc4756e4b7e65ff7633de39a0576d7726b4c680
smsource <58534618+smsource@users.noreply.github.com>
2020-04-18 07:25:33 +0800

    Add files via upload

--- diy-newifi3.sh
+++ diy-newifi3.sh
@@ -1,6 +1,9 @@
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
+echo '修改网关地址'
+sed -i 's/192.168.1.1/192.168.18.254/g' package/base-files/files/bin/config_generate
+
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner-newifi3 package/base-files/files/etc/banner
